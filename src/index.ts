9bf098dfd15a63ea4c7ec9fdb3e2a78584f1d7ae
Andrey Sakharov <a.sakharov@biletix.ru>
2020-04-30 00:27:25 +0300

    main logic was implemented

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import type { ComponentClass } from 'react';
+const { createElement } = require('react');
+const { renderToStaticMarkup } = require('react-dom/server');
+const { resolve, join } = require('path');
+
+
+const engineOptions = {
+   babel: {
+      presets: [
+         '@babel/preset-react',
+         [
+            '@babel/preset-env',
+            {
+               targets: {
+                  node: 'current',
+               },
+            },
+         ],
+      ],
+      plugins: ['@babel/transform-flow-strip-types'],
+   },
+};
+
+
+const engineFactory = (templateRoot: string) =>
+{
+   const templateDir = resolve(templateRoot);
+
+   require('@babel/register')(
+      Object.assign({only: ([] as string[]).concat(templateDir)}, engineOptions.babel)
+   );
+
+   return <P extends {}>(templateName: string, props: P) =>
+   {
+      const filePath = join(templateDir, templateName);
+
+      let component: ComponentClass<P, {}> = require(filePath);
+      // Transpiled ES6 may export components as { default: Component }
+      component = 'default' in component ? component['default'] : component;
+
+      const markup = '<!DOCTYPE html>' + renderToStaticMarkup(
+         createElement(component, props),
+      );
+
+      return markup;
+   };
+};
+
+export default engineFactory;
+module.exports = engineFactory;
